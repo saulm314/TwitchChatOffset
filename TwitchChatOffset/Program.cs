e4153a1e1e2cce31ffec77fc5b5cc185bc6250a3
Saulius Markevicius <smsmvicious@gmail.com>
2025-04-06 15:44:51 +0100

    Add basic program entry

--- TwitchChatOffset/Program.cs
+++ TwitchChatOffset/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Running Twitch Chat Offset...");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please provide arguments: [input json path] [output json path] [offset (seconds)]");
+                return;
+            }
+
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Unexpected number of arguments");
+                return;
+            }
+
+            string inputPath = args[0];
+            string outputPath = args[1];
+            bool offsetIsInt = int.TryParse(args[2], out int offset);
+
+            if (!offsetIsInt)
+            {
+                Console.WriteLine("Integer expected for offset");
+                return;
+            }
         }
     }
 }
