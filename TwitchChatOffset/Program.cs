578770c872da702ac05c4f860a47aaf4af04f559
Saulius Markevicius <smsmvicious@gmail.com>
2025-04-21 23:47:10 +0100

    Change namespace declaration to be file-scoped

--- TwitchChatOffset/Program.cs
+++ TwitchChatOffset/Program.cs
@@ -3,74 +3,73 @@ using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace TwitchChatOffset
+namespace TwitchChatOffset;
+
+internal class Program
 {
-    internal class Program
+    const string Version = "1.1.0";
+
+    static void Main(string[] args)
     {
-        const string Version = "1.1.0";
-
-        static void Main(string[] args)
+        Console.WriteLine($"Running TwitchChatOffset {Version}");
+        if (args.Length == 0)
         {
-            Console.WriteLine($"Running TwitchChatOffset {Version}");
-            if (args.Length == 0)
-            {
-                Console.WriteLine("Please provide arguments: <input json path> <output json path> <start (seconds)> [end (seconds)]");
-                return;
-            }
-
-            if (args.Length != 3 && args.Length != 4)
-            {
-                Console.WriteLine("Unexpected number of arguments");
-                return;
-            }
-
-            string inputPath = args[0];
-            string outputPath = args[1];
-            bool startIsLong = long.TryParse(args[2], out long start);
-            long end = -1;
-
-            if (!startIsLong)
-            {
-                Console.WriteLine("Integer expected for start");
-                return;
-            }
-
-            if (args.Length == 4)
-            {
-                bool endIsLong = long.TryParse(args[3], out end);
-                if (!endIsLong)
-                {
-                    Console.WriteLine("Integer expected for end");
-                    return;
-                }
-            }
-
-            JToken parent = (JToken)JsonConvert.DeserializeObject(File.ReadAllText(inputPath))!;
-            JArray comments = (JArray)parent["comments"]!;
-            int i = 0;
-            while (i < comments.Count)
-            {
-                JToken comment = comments[i];
-                JValue commentOffset = (JValue)comment["content_offset_seconds"]!;
-                long commentOffsetValue = (long)commentOffset.Value!;
-                if (commentOffsetValue < start)
-                {
-                    comments.RemoveAt(i);
-                    continue;
-                }
-                if (end != -1 && commentOffsetValue > end)
-                {
-                    comments.RemoveAt(i);
-                    continue;
-                }
-                commentOffset.Value = commentOffsetValue - start;
-                i++;
-            }
-
-            string output = JsonConvert.SerializeObject(parent);
-            File.WriteAllText(outputPath, output);
-
-            Console.WriteLine("Done.");
+            Console.WriteLine("Please provide arguments: <input json path> <output json path> <start (seconds)> [end (seconds)]");
+            return;
         }
+
+        if (args.Length != 3 && args.Length != 4)
+        {
+            Console.WriteLine("Unexpected number of arguments");
+            return;
+        }
+
+        string inputPath = args[0];
+        string outputPath = args[1];
+        bool startIsLong = long.TryParse(args[2], out long start);
+        long end = -1;
+
+        if (!startIsLong)
+        {
+            Console.WriteLine("Integer expected for start");
+            return;
+        }
+
+        if (args.Length == 4)
+        {
+            bool endIsLong = long.TryParse(args[3], out end);
+            if (!endIsLong)
+            {
+                Console.WriteLine("Integer expected for end");
+                return;
+            }
+        }
+
+        JToken parent = (JToken)JsonConvert.DeserializeObject(File.ReadAllText(inputPath))!;
+        JArray comments = (JArray)parent["comments"]!;
+        int i = 0;
+        while (i < comments.Count)
+        {
+            JToken comment = comments[i];
+            JValue commentOffset = (JValue)comment["content_offset_seconds"]!;
+            long commentOffsetValue = (long)commentOffset.Value!;
+            if (commentOffsetValue < start)
+            {
+                comments.RemoveAt(i);
+                continue;
+            }
+            if (end != -1 && commentOffsetValue > end)
+            {
+                comments.RemoveAt(i);
+                continue;
+            }
+            commentOffset.Value = commentOffsetValue - start;
+            i++;
+        }
+
+        string output = JsonConvert.SerializeObject(parent);
+        File.WriteAllText(outputPath, output);
+
+        Console.WriteLine("Done.");
     }
 }
