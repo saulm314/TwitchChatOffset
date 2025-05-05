fd926b7f61b0c0d8a80407c1e34ebfe4bdb86a4b
Saulius Markevicius <smsmvicious@gmail.com>
2025-05-05 02:43:54 +0100

    Add automatic browser opening

--- TwitchChatOffset/GetVideosHandler.cs
+++ TwitchChatOffset/GetVideosHandler.cs
@@ -1,9 +1,29 @@
-﻿namespace TwitchChatOffset;
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace TwitchChatOffset;
 
 public static class GetVideosHandler
 {
     public static void HandleGetVideos(string user, string outputPath, GetVideosFormatting formatting)
     {
-        System.Console.WriteLine($"{user} {outputPath} {formatting}");
+        Console.WriteLine("Opening browser...");
+        try
+        {
+            Process process = new();
+            ProcessStartInfo startInfo = new(Url)
+            {
+                UseShellExecute = true
+            };
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+        catch (Win32Exception) { }
+        catch (PlatformNotSupportedException) { }
+        Console.WriteLine($"If browser failed to open, then open it manually and copy-and-paste the following URL:\n{Url}");
     }
+
+    private const string Url =
+        "https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=wt92v33cpoj7s7ccx0u3s1klz1oadl&redirect_uri=https://localhost:32100";
 }
