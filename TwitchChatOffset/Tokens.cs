1062b62b4188e0ccd646ca8bd8b6a71253ea09b6
Saulius Markevicius <smsmvicious@gmail.com>
2025-05-19 16:49:49 +0100

    Add `--output-directory` alias to `--output` option

--- TwitchChatOffset/Tokens.cs
+++ TwitchChatOffset/Tokens.cs
@@ -10,6 +10,6 @@ public static class Tokens
     public static Option<long> StartOption => new("--start", () => 0, "Starting point in seconds before which to dismiss chat messages (optional)");
     public static Option<long> EndOption => new("--end", () => -1, "Ending point in seconds after which to dismiss chat messages (optional)");
     public static Option<Format> FormatOption => new(["--format", "--formatting", "-f"], () => default, "Format for the output file (optional)");
-    public static Option<string> OutputDirOption => new(["--output", "-o"], () => ".", "Output directory (will create if doesn't exist) (optional)");
+    public static Option<string> OutputDirOption => new(["--output", "--output-directory", "-o"], () => ".", "Output directory (will create if doesn't exist) (optional)");
     public static Option<bool> QuietOption => new(["--quiet", "-q"], () => false, "Do not print a message after each file is written (optional)");
 }
