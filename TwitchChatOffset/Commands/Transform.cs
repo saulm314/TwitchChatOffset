9b2cbaab17aea4584a452f178d93d020cc0556ed
Saulius Markevicius <smsmvicious@gmail.com>
2025-05-31 20:43:59 +0100

    Fix Transform class

--- TwitchChatOffset/Commands/Transform.cs
+++ TwitchChatOffset/Commands/Transform.cs
@@ -1,14 +1,33 @@
 ﻿using System.CommandLine;
+using System.CommandLine.Binding;
 
 namespace TwitchChatOffset.Commands;
 
-public class Transform
+public class Transform : BinderBase<Transform.Data>
 {
-    public readonly Argument<string> inputArgument = Tokens.InputArgument;
-    public readonly Argument<string> outputArgument = Tokens.OutputArgument;
-    public readonly Option<long> startOption = Tokens.StartOption;
-    public readonly Option<long> endOption = Tokens.EndOption;
-    public readonly Option<Format> formatOption = Tokens.FormatOption;
+    public Command Add(Command parentCommand)
+    {
+        Command command = new("transform", "Transform a JSON Twitch chat file and put the new contents in a new file");
+        parentCommand.Add(command);
+        command.SetHandler(Handle, this);
+        return command;
+    }
+
+    private readonly Argument<string> inputArgument = Tokens.InputArgument;
+    private readonly Argument<string> outputArgument = Tokens.OutputArgument;
+    private readonly Option<long> startOption = Tokens.StartOption;
+    private readonly Option<long> endOption = Tokens.EndOption;
+    private readonly Option<Format> formatOption = Tokens.FormatOption;
+
+    protected override Data GetBoundValue(BindingContext bindingContext)
+        => new
+        (
+            bindingContext.ParseResult.GetValueForArgument(inputArgument),
+            bindingContext.ParseResult.GetValueForArgument(outputArgument),
+            bindingContext.ParseResult.GetValueForOption(startOption),
+            bindingContext.ParseResult.GetValueForOption(endOption),
+            bindingContext.ParseResult.GetValueForOption(formatOption)
+        );
 
     public readonly record struct Data
     (
@@ -19,7 +38,7 @@ public class Transform
         Format Format
     );
 
-    public void Handle(Data data)
+    private void Handle(Data data)
     {
         (string inputPath, string outputPath, long start, long end, Format format) = data;
         TransformHandler.HandleTransform(inputPath, outputPath, start, end, format);
