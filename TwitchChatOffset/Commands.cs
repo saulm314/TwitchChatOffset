5b7b67d1c8f57ccbfff9acc6196a81f754f48b04
Saulius Markevicius <smsmvicious@gmail.com>
2025-05-31 20:18:40 +0100

    Delete Commands.cs file

--- TwitchChatOffset/Commands.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TwitchChatOffset;
-
-public readonly record struct Transform(string InputPath, string OutputPath, long Start, long End, Format PFormat);
-public readonly record struct TransformManyToMany(string CsvPath, string OutputDir, Format PFormat, bool Quiet);
-public readonly record struct TransformOneToMany(string InputPath, string CsvPath, string OutputDir, Format PFormat, bool Quiet);
-public readonly record struct TransformAll(string Suffix, string InputDir, string SearchPattern, string OutputDir, Format PFormat, bool Quiet,
-    long Start, long End);
