47efe601c1a31abb7f1653196e57e4c5a99bca92
Saulius Markevicius <smsmvicious@gmail.com>
2025-06-02 01:23:15 +0100

    Finalise abstraction for all bulk processing

--- TwitchChatOffset/BulkTransform.cs
+++ TwitchChatOffset/BulkTransform.cs
@@ -12,19 +12,20 @@ public static class BulkTransform
 {
     public static void HandleTransformManyToMany(string csvPath, long start, long end, Format format, string outputDir, bool quiet)
     {
+        BulkTransform.quiet = quiet;
         Dictionary<string, CField> optionMap = [];
         AliasesCFieldPair[] pairs =
         [
-            new(["input-file", "inputFile"],    CField.New(typeof(TransformManyToManyCsv).GetField(nameof(TransformManyToManyCsv.inputFile))!)),
-            new(["output-file", "outputFile"],  CField.New(typeof(BulkTransformCsv).GetField(nameof(BulkTransformCsv.outputFile))!)),
-            new(Tokens.StartOptionAliases,      CField.New(typeof(BulkTransformCsv).GetField(nameof(BulkTransformCsv.start))!)),
-            new(Tokens.EndOptionAliases,        CField.New(typeof(BulkTransformCsv).GetField(nameof(BulkTransformCsv.end))!)),
-            new(Tokens.FormatOptionAliases,     CField.New(typeof(BulkTransformCsv).GetField(nameof(BulkTransformCsv.format))!)),
-            new(Tokens.OutputDirOptionAliases,  CField.New(typeof(BulkTransformCsv).GetField(nameof(BulkTransformCsv.outputDir))!))
+            new(["--input-file", "--inputFile"],    CField.New(typeof(TransformManyToManyCsv).GetField(nameof(TransformManyToManyCsv.inputFile))!)),
+            new(["--output-file", "--outputFile"],  CField.New(typeof(BulkTransformCsv).GetField(nameof(BulkTransformCsv.outputFile))!)),
+            new(Tokens.StartOptionAliases,          CField.New(typeof(BulkTransformCsv).GetField(nameof(BulkTransformCsv.start))!)),
+            new(Tokens.EndOptionAliases,            CField.New(typeof(BulkTransformCsv).GetField(nameof(BulkTransformCsv.end))!)),
+            new(Tokens.FormatOptionAliases,         CField.New(typeof(BulkTransformCsv).GetField(nameof(BulkTransformCsv.format))!)),
+            new(Tokens.OutputDirOptionAliases,      CField.New(typeof(BulkTransformCsv).GetField(nameof(BulkTransformCsv.outputDir))!))
         ];
         AddAliasesToOptionMap(optionMap, pairs);
         IEnumerable<TransformManyToManyCsv> data = GetProcessedLines(csvPath, optionMap, start, end, format, outputDir);
-        WriteFiles(data, quiet);
+        WriteFiles(data);
     }
 
     private static void AddAliasesToOptionMap(Dictionary<string, CField> optionMap, AliasesCFieldPair[] pairs)
@@ -37,70 +38,115 @@ public static class BulkTransform
     private static IEnumerable<TransformManyToManyCsv> GetProcessedLines(string csvPath, Dictionary<string, CField> optionMap,
         long start, long end, Format format, string outputDir)
     {
+        GetHeadersAndLines(csvPath, out string[] headers, out IEnumerable<string[]> lines);
+        return GetProcessedLines(lines, headers, optionMap, start, end, format, outputDir);
+    }
+
+    private static IEnumerable<BulkTransformCsv> GetProcessedLines(string csvPath, string inputFile, Func<BulkTransformCsv> newOptions,
+        Dictionary<string, CField> optionMap, long start, long end, Format format, string outputDir)
+    {
+        GetHeadersAndLines(csvPath, out string[] headers, out IEnumerable<string[]> lines);
+        return GetProcessedLines(lines, inputFile, newOptions, headers, optionMap, start, end, format, outputDir);
+    }
+
+    private static void GetHeadersAndLines(string csvPath, out string[] headers, out IEnumerable<string[]> lines)
+    {
         CSVReader reader = CSVReader.FromFile(csvPath, csvSettings);
+        headers = GetDashedHeaders(reader);
+        lines = reader.Lines();
+    }
+
+    private static string[] GetDashedHeaders(CSVReader reader)
+    {
         string[] dashedHeaders = new string[reader.Headers.Length];
-        for (int i = 0; i < reader.Headers.Length; i++)
+        for (int i = 0; i < dashedHeaders.Length; i++)
             dashedHeaders[i] = "--" + reader.Headers[i];
-        IEnumerable<string[]> lines = reader.Lines();
+        return dashedHeaders;
+    }
+
+    private static IEnumerable<TransformManyToManyCsv> GetProcessedLines(IEnumerable<string[]> lines, string[] headers, Dictionary<string, CField> optionMap,
+        long start, long end, Format format, string outputDir)
+    {
         foreach (string[] line in lines)
         {
             TransformManyToManyCsv options = new();
-            for (int i = 0; i < line.Length; i++)
-            {
-                string? key =
-                    optionMap.ContainsKey(reader.Headers[i]) ? reader.Headers[i] :
-                    optionMap.ContainsKey(dashedHeaders[i]) ? dashedHeaders[i] : null;
-                if (key == null)
-                    continue;
-                CField cfield = optionMap[key];
-                if (line[i] == string.Empty)
-                    continue;
-                if (!cfield.Converter.IsValid(line[i]))
-                {
-                    WriteWarning($"Cannot convert \"{line[i]}\" to type {cfield.Field.FieldType.FullName}; treating as an empty string...", 2);
-                    continue;
-                }
-                object? value = cfield.Converter.ConvertFromString(line[i]);
-                cfield.Field.SetValue(options, value);
-            }
-            if (options.inputFile == null)
-            {
-                WriteError($"Input file must not be empty! Skipping...", 2);
-                continue;
-            }
-            if (options.outputFile == null)
-            {
-                WriteError($"Output file must not be empty! Skipping...", 2);
-                continue;
-            }
-            options.start ??= start;
-            options.end ??= end;
-            options.format ??= format;
-            options.outputDir ??= outputDir;
-            yield return options;
+            bool valid = GetProcessedLine(options, line, headers, optionMap, start, end, format, outputDir);
+            if (valid)
+                yield return options;
         }
     }
 
-    private static void WriteFiles(IEnumerable<BulkTransformCsv> lines, string inputFile, bool quiet)
+    private static IEnumerable<BulkTransformCsv> GetProcessedLines(IEnumerable<string[]> lines, string inputFile, Func<BulkTransformCsv> newOptions,
+        string[] headers, Dictionary<string, CField> optionMap, long start, long end, Format format, string outputDir)
     {
-        WriteLine("Writing files...");
-        foreach (BulkTransformCsv line in lines)
-            WriteFile(line, inputFile, quiet);
-        WriteLine("Done.");
+        foreach (string[] line in lines)
+        {
+            BulkTransformCsv options = newOptions();
+            bool valid = GetProcessedLine(options, in inputFile, line, headers, optionMap, start, end, format, outputDir);
+            if (valid)
+                yield return options;
+        }
     }
 
-    private static void WriteFiles(IEnumerable<TransformManyToManyCsv> lines, bool quiet)
-    {
-        WriteLine("Writing files...");
-        foreach (TransformManyToManyCsv line in lines)
-            WriteFile(line, line.inputFile!, quiet);
-        WriteLine("Done.");
-    }
+    private static bool GetProcessedLine(TransformManyToManyCsv options, string[] line, string[] headers, Dictionary<string, CField> optionMap,
+        long start, long end, Format format, string outputDir)
+        => GetProcessedLine(options, in options.inputFile, line, headers, optionMap, start, end, format, outputDir);
 
-    private static void WriteFile(BulkTransformCsv line, string inputFile, bool quiet)
+    // returns false if line is invalid
+    private static bool GetProcessedLine(BulkTransformCsv options, in string? inputFile, string[] line, string[] headers, Dictionary<string, CField> optionMap,
+        long start, long end, Format format, string outputDir)
     {
+        for (int i = 0; i < line.Length; i++)
+            WriteField(options, line[i], headers[i], optionMap);
+        if (inputFile == null)
+        {
+            WriteError($"Input file must not be empty! Skipping...", 2);
+            return false;
+        }
+        if (options.outputFile == null)
+        {
+            WriteError($"Output file must not be empty! Skipping...", 2);
+            return false;
+        }
         if (!quiet)
-            WriteLine($"{line.outputFile}", 1);
+            WriteLine($"{options.outputFile}", 1);
+        options.start ??= start;
+        options.end ??= end;
+        options.format ??= format;
+        options.outputDir ??= outputDir;
+        return true;
+    }
+
+    private static void WriteField(BulkTransformCsv options, string field, string header, Dictionary<string, CField> optionMap)
+    {
+        if (!optionMap.TryGetValue(header, out CField cField))
+            return;
+        if (field == string.Empty)
+            return;
+        if (!cField.Converter.IsValid(field))
+        {
+            WriteWarning($"Cannot convert \"{field}\" to type {cField.Field.FieldType.FullName}; treating as an empty string...", 2);
+            return;
+        }
+        object? value = cField.Converter.ConvertFromString(field);
+        cField.Field.SetValue(options, value);
+    }
+
+    private static void WriteFiles(IEnumerable<TransformManyToManyCsv> lines)
+    {
+        foreach (TransformManyToManyCsv line in lines)
+            WriteFile(line, line.inputFile!);
+        WriteLine("Done.", 0, quiet);
+    }
+
+    private static void WriteFiles(IEnumerable<BulkTransformCsv> lines, string inputFile)
+    {
+        foreach (BulkTransformCsv line in lines)
+            WriteFile(line, inputFile);
+    }
+
+    private static void WriteFile(BulkTransformCsv line, string inputFile)
+    {
         _ = Directory.CreateDirectory(line.outputDir!);
         string outputPath = line.outputDir!.EndsWith('\\') ? line.outputDir + line.outputFile : line.outputDir + '\\' + line.outputFile;
         try
@@ -125,11 +171,10 @@ public static class BulkTransform
         IEnumerable<TransformOneToManyCsv> data = CSV.Deserialize<TransformOneToManyCsv>(File.ReadAllText(csvPath), csvSettings);
         string input = File.ReadAllText(inputPath);
         JToken parent = (JToken)JsonConvert.DeserializeObject(input)!;
-        WriteLine("Writing files...");
+        WriteLine("Writing files...", 0, quiet);
         foreach (TransformOneToManyCsv line in data)
         {
-            if (!quiet)
-                WriteLine($"{line.outputFile}", 1);
+            WriteLine($"{line.outputFile}", 1, quiet);
             string outputPath = outputDir.EndsWith('\\') ? outputDir + line.outputFile : outputDir + '\\' + line.outputFile;
             JToken clonedParent = parent.DeepClone();
             Transform.HandleTransform(clonedParent, outputPath, line.start, line.end, format);
@@ -140,9 +185,8 @@ public static class BulkTransform
     {
         _ = Directory.CreateDirectory(outputDir);
         string[] fileNames = Directory.GetFiles(inputDir, searchPattern);
-        if (!quiet)
-            WriteEnumerable(fileNames, "Input files found:");
-        WriteLine("Writing files...");
+        WriteEnumerable(fileNames, "Input files found:", 0, quiet);
+        WriteLine("Writing files...", 0, quiet);
         foreach (string fileName in fileNames)
         {
             string fileNameBody = Path.GetFileNameWithoutExtension(fileName);
@@ -154,8 +198,7 @@ public static class BulkTransform
             outputPathBuilder.Append(fileNameBody);
             outputPathBuilder.Append(suffix);
             string outputPath = outputPathBuilder.ToString();
-            if (!quiet)
-                WriteLine($"{outputPath}", 2);
+            WriteLine($"{outputPath}", 2, quiet);
 
             try
             {
@@ -178,4 +221,6 @@ public static class BulkTransform
     {
         FieldDelimiter = ','
     };
+
+    private static bool quiet = false;
 }
--- TwitchChatOffset/ConsoleUtils.cs
+++ TwitchChatOffset/ConsoleUtils.cs
@@ -6,56 +6,60 @@ namespace TwitchChatOffset;
 
 public static class ConsoleUtils
 {
-    public static void WriteLine(object? message, byte indent = 0)
+    public static void WriteLine(object? message, byte indent = 0, bool quiet = false)
     {
+        if (quiet)
+            return;
         Span<char> indentChars = stackalloc char[indent];
         indentChars.Fill('\t');
         string indentStr = new(indentChars);
         Console.WriteLine(indentStr + message);
     }
 
-    public static void Write(object? message, byte indent = 0)
+    public static void Write(object? message, byte indent = 0, bool quiet = false)
     {
+        if (quiet)
+            return;
         Span<char> indentChars = stackalloc char[indent];
         indentChars.Fill('\t');
         string indentStr = new(indentChars);
         Console.Write(indentStr + message);
     }
 
-    public static void WriteError(object? message, byte indent = 0)
+    public static void WriteError(object? message, byte indent = 0, bool quiet = false)
     {
         ConsoleColor originalColor = Console.ForegroundColor;
         Console.ForegroundColor = ConsoleColor.Red;
-        WriteLine(message, indent);
+        WriteLine(message, indent, quiet);
         Console.ForegroundColor = originalColor;
     }
 
-    public static void WriteWarning(object? message, byte indent = 0)
+    public static void WriteWarning(object? message, byte indent = 0, bool quiet = false)
     {
         ConsoleColor originalColor = Console.ForegroundColor;
         Console.ForegroundColor = ConsoleColor.Yellow;
-        WriteLine(message, indent);
+        WriteLine(message, indent, quiet);
         Console.ForegroundColor = originalColor;
     }
 
-    public static void WriteEnumerable(IEnumerable enumerable, string? heading = null, byte indent = 0)
+    public static void WriteEnumerable(IEnumerable enumerable, string? heading = null, byte indent = 0, bool quiet = false)
     {
         if (heading != null)
-            WriteLine(heading, indent);
+            WriteLine(heading, indent, quiet);
         foreach (object? item in enumerable)
-            WriteLine($"\t{item}", indent);
+            WriteLine($"\t{item}", indent, quiet);
     }
 
-    public static void WriteType(Type type, object? obj, string? heading = null, byte indent = 0)
+    public static void WriteType(Type type, object? obj, string? heading = null, byte indent = 0, bool quiet = false)
     {
         if (heading != null)
-            WriteLine(heading, indent);
+            WriteLine(heading, indent, quiet);
         foreach (FieldInfo field in type.GetFields())
-            WriteLine($"\tField:\t{field.Name} = {field.GetValue(obj)}", indent);
+            WriteLine($"\tField:\t{field.Name} = {field.GetValue(obj)}", indent, quiet);
         foreach (PropertyInfo property in type.GetProperties())
-            WriteLine($"\tProperty:\t{property.Name} = {property.GetValue(obj)}", indent);
+            WriteLine($"\tProperty:\t{property.Name} = {property.GetValue(obj)}", indent, quiet);
     }
 
-    public static void WriteObjectMembers(object obj, string? heading = null, byte indent = 0)
-        => WriteType(obj.GetType(), obj, heading, indent);
+    public static void WriteObjectMembers(object obj, string? heading = null, byte indent = 0, bool quiet = false)
+        => WriteType(obj.GetType(), obj, heading, indent, quiet);
 }
