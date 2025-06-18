b09eba51afab91702f1a26a4970d94bbea4f0c88
Saulius Markevicius <smsmvicious@gmail.com>
2025-06-18 14:53:03 +0100

    Add TransformManyToManyCsv class

---
+++ TwitchChatOffset/TransformManyToManyCsv.cs
@@ -0,0 +1,24 @@
+﻿using static TwitchChatOffset.OptionAliases;
+
+namespace TwitchChatOffset;
+
+public class TransformManyToManyCsv
+{
+    [Aliases(["input-file", "inputFile"])]
+    public string? inputFile;
+
+    [Aliases(["output-file", "outputFile"])]
+    public string? outputFile;
+    
+    [Aliases(typeof(OptionAliases), nameof(Start))]
+    public long? start;
+    
+    [Aliases(typeof(OptionAliases), nameof(End))]
+    public long? end;
+    
+    [Aliases(typeof(OptionAliases), nameof(PFormat))]
+    public Format? format;
+    
+    [Aliases(typeof(OptionAliases), nameof(OutputDir))]
+    public string? outputDir;
+}
