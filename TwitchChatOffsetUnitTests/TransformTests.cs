fceafece0c3c76b79a1e40f6057bb1b00c333a5b
Saulius Markevicius <smsmvicious@gmail.com>
2025-07-26 23:47:11 +0100

    Fix test method name

--- TwitchChatOffsetUnitTests/TransformTests.cs
+++ TwitchChatOffsetUnitTests/TransformTests.cs
@@ -34,7 +34,7 @@ public class TransformTests
     [InlineData(long.MaxValue, long.MaxValue, Format.Json)]
     [InlineData(long.MaxValue, long.MaxValue, Format.JsonIndented)]
     [InlineData(long.MaxValue, long.MaxValue, Format.Plaintext)]
-    public void MTransformString_EmptyString_ThrowsJsonContentException(long start, long end, Format format)
+    public void MTransformString_EmptyString_ThrowsJsonContentExceptionEmpty(long start, long end, Format format)
     {
         string input = string.Empty;
 
