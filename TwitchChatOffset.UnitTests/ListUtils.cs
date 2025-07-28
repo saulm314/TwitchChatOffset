5bf13ba3cc33d6068a948afc12b76dcde32d0565
Saulius Markevicius <smsmvicious@gmail.com>
2025-07-28 13:11:10 +0100

    Add ListUtils class

---
+++ TwitchChatOffset.UnitTests/ListUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TwitchChatOffsetUnitTests;
+
+public static class ListUtils
+{
+    public static int Max(int int1, params int[] ints)
+    {
+        int max = int1;
+        foreach (int i in ints)
+            if (i > max)
+                max = i;
+        return max;
+    }
+
+    public static int MaxLength(ICollection collection1, params ICollection[] collections)
+    {
+        int max = collection1.Count;
+        foreach (ICollection collection in collections)
+            if (collection.Count > max)
+                max = collection.Count;
+        return max;
+    }
+
+    public static T IthOrLast<T>(this IList<T> list, int i) => i < list.Count ? list[i] : list[^1];
+
+    public static IEnumerable<T> Transform<T>(this IEnumerable<T> enumerable, Action<T> transform)
+    {
+        foreach (T item in enumerable)
+            transform(item);
+        return enumerable;
+    }
+}
