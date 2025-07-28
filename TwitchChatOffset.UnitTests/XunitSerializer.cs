112cfcd9a7b1fe053d04754a5cff1728a2b1435a
Saulius Markevicius <smsmvicious@gmail.com>
2025-07-28 18:26:39 +0100

    Add XunitSerializer

---
+++ TwitchChatOffset.UnitTests/XunitSerializer.cs
@@ -0,0 +1,30 @@
+﻿using TwitchChatOffset.UnitTests;
+using Xunit.Sdk;
+using System;
+using Newtonsoft.Json;
+
+[assembly: RegisterXunitSerializer(typeof(XunitSerializer), typeof(CsvSerializationTests.DeserializeTestData))]
+
+namespace TwitchChatOffset.UnitTests;
+
+public class XunitSerializer : IXunitSerializer
+{
+    public bool IsSerializable(Type type, object? value, out string failureReason)
+    {
+        failureReason = string.Empty;
+        if (type == typeof(CsvSerializationTests.DeserializeTestData))
+            return true;
+        failureReason = $"Type {type} not supported for serialization";
+        return false;
+    }
+
+    public object Deserialize(Type type, string serializedValue)
+    {
+        return JsonConvert.DeserializeObject(serializedValue, type)!;
+    }
+
+    public string Serialize(object value)
+    {
+        return JsonConvert.SerializeObject(value);
+    }
+}
