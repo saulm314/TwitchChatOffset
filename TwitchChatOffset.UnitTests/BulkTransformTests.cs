85b62e7ca8bff130c1e7af8f82fa816a4f4644fe
Saulius Markevicius <smsmvicious@gmail.com>
2025-07-28 12:56:29 +0100

    Add ResolveClashPrioritiseCsv struct test

--- TwitchChatOffset.UnitTests/BulkTransformTests.cs
+++ TwitchChatOffset.UnitTests/BulkTransformTests.cs
@@ -57,4 +57,20 @@ public class BulkTransformTests
 
         Assert.Equal(expectedOutput, output);
     }
+
+    [Theory]
+    [InlineData((long)5, 2, false, 5)]
+    [InlineData((long)2, 5, false, 2)]
+    [InlineData(null, 2, false, 2)]
+    [InlineData((long)5, 2, true, 5)]
+    [InlineData((long)2, 5, true, 2)]
+    [InlineData(null, 2, true, 2)]
+    public void ResolveClashPrioritiseCsvStructTest(long? csvValue, long cliValue, bool cliValueSpecified, long expectedOutput)
+    {
+        MockNullableOption<long> cliOption = new(cliValue, cliValueSpecified);
+
+        long output = BulkTransform.ResolveClashPrioritiseCsv(csvValue, cliOption);
+
+        Assert.Equal(expectedOutput, output);
+    }
 }
---
+++ TwitchChatOffset.UnitTests/MockNullableOption.cs
@@ -0,0 +1,9 @@
+﻿using TwitchChatOffset.CommandLine.Options;
+
+namespace TwitchChatOffset.UnitTests;
+
+public class MockNullableOption<TType>(TType value, bool valueSpecified) : INullableOption<TType> where TType : notnull
+{
+    public TType Value => value;
+    public bool ValueSpecified => valueSpecified;
+}
