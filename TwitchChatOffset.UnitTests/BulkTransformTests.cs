2b8412f3d90e5da3f1b0094e654df57eae1d3a37
Saulius Markevicius <smsmvicious@gmail.com>
2025-07-28 12:58:16 +0100

    Add ResolveClashPrioritiseCsv class test

--- TwitchChatOffset.UnitTests/BulkTransformTests.cs
+++ TwitchChatOffset.UnitTests/BulkTransformTests.cs
@@ -73,4 +73,20 @@ public class BulkTransformTests
 
         Assert.Equal(expectedOutput, output);
     }
+
+    [Theory]
+    [InlineData("5", "2", false, "5")]
+    [InlineData("2", "5", false, "2")]
+    [InlineData(null, "2", false, "2")]
+    [InlineData("5", "2", true, "5")]
+    [InlineData("2", "5", true, "2")]
+    [InlineData(null, "2", true, "2")]
+    public void ResolveClashPrioritiseCsvClassTest(string? csvValue, string cliValue, bool cliValueSpecified, string expectedOutput)
+    {
+        MockNullableOption<string> cliOption = new(cliValue, cliValueSpecified);
+
+        string output = BulkTransform.ResolveClashPrioritiseCsv(csvValue, cliOption);
+
+        Assert.Equal(expectedOutput, output);
+    }
 }
