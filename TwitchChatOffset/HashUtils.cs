ee943587d68ed9e02ff5bca4767d767a4a8d470a
Saulius Markevicius <smsmvicious@gmail.com>
2025-07-29 17:05:36 +0100

    Add another HashUtils function

--- TwitchChatOffset/HashUtils.cs
+++ TwitchChatOffset/HashUtils.cs
@@ -5,7 +5,7 @@ namespace TwitchChatOffset;
 public static class HashUtils
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static int GetHashCode<T>(T value) where T : struct => value.GetHashCode();
+    public static int GetHashCode<T>(T value) where T : notnull => value.GetHashCode();
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static int GetHashCode<T>(T? nullable, int defaultHash) where T : struct => nullable.HasValue ? nullable.Value.GetHashCode() : defaultHash;
