9b4754b2a175d39b3c3945e6fe9c307cfebcb81b
Saulius Markevicius <smsmvicious@gmail.com>
2025-08-02 20:31:00 +0100

    Add different order CSV test

--- TwitchChatOffset.UnitTests/CsvSerializationTests.cs
+++ TwitchChatOffset.UnitTests/CsvSerializationTests.cs
@@ -384,6 +384,40 @@ public class CsvSerializationTests
                 new(null, null, null, 0, null, null, null, null, "1")
             ]
         ));
+
+        yield return new(new
+        (
+            "different order",
+            """
+            string,long-unstripped,--long-stripped,longObjectDefault,long-unstripped,long-stripped,--long-unstripped,long-non-nullable,bool,char,double,mock-enum
+            ,19,20,,17,,,,,,,
+            Hello World!,19,20,0,17,0,0,0,false,a,0.0,PascalCase
+            Goodbye!,19,20,1,17,2,3,4,true,A,1.2,camelCase
+            Goodbye!,19,20,-1,17,-2,-3,-4,False,0,-0.0,snake_case
+            Goodbye!,19,20,123456,17,234567,345678,456789,True, ,-1.2,pascalcase
+            Goodbye!,19,20,123456,17,234567,345678,456789,True, ,12345.6,CAMELCASE
+            Goodbye!,19,20,123456,17,234567,345678,456789,True, ,-12345.6,SnAkE_cAsE
+            Goodbye!,19,20,123456,17,234567,345678,456789,True, ,0,SnAkE_cAsE
+            Goodbye!,19,20,123456,17,234567,345678,456789,True, ,5,SnAkE_cAsE
+            Goodbye!,19,20,123456,17,234567,345678,456789,True, ,-5,SnAkE_cAsE
+            1,19,20,hello,17,51.0,true, ,yes,two,hello,snakecase
+            1,19,20,5a,17,a5,0.0,5f,no,three,5.0f,snakecase
+            """,
+            [
+                new(null, null, null, 0, null, null, null, null, null),
+                new(0, 0, 0, 0, false, 'a', 0.0, MockEnum.PascalCase, "Hello World!"),
+                new(1, 2, 3, 4, true, 'A', 1.2, MockEnum.camelCase, "Goodbye!"),
+                new(-1, -2, -3, -4, false, '0', -0.0, MockEnum.snake_case, "Goodbye!"),
+                new(123456, 234567, 345678, 456789, true, ' ', -1.2, MockEnum.PascalCase, "Goodbye!"),
+                new(123456, 234567, 345678, 456789, true, ' ', 12345.6, MockEnum.camelCase, "Goodbye!"),
+                new(123456, 234567, 345678, 456789, true, ' ', -12345.6, MockEnum.snake_case, "Goodbye!"),
+                new(123456, 234567, 345678, 456789, true, ' ', 0.0, MockEnum.snake_case, "Goodbye!"),
+                new(123456, 234567, 345678, 456789, true, ' ', 5.0, MockEnum.snake_case, "Goodbye!"),
+                new(123456, 234567, 345678, 456789, true, ' ', -5.0, MockEnum.snake_case, "Goodbye!"),
+                new(null, null, null, 0, null, null, null, null, "1"),
+                new(null, null, null, 0, null, null, null, null, "1")
+            ]
+        ));
     }
 
     public record DeserializeTestData(string testName, string CsvString, MockCsvObject[] ExpectedCsvObjects);
