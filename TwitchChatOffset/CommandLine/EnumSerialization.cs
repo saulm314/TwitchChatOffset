88510ca9734749930f7539fbbdd07296792899ba
Saulius Markevicius <saulius0412@gmail.com>
2025-10-22 06:16:26 +0100

    Add enum helpers in command line descriptions

---
+++ TwitchChatOffset/CommandLine/EnumSerialization.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TwitchChatOffset.CommandLine;
+
+public static class EnumSerialization
+{
+    public static string Serialize<T>() where T : struct, Enum => '[' + string.Join('|', Enum.GetNames<T>()) + ']';
+}
--- TwitchChatOffset/CommandLine/Tokens.cs
+++ TwitchChatOffset/CommandLine/Tokens.cs
@@ -3,6 +3,7 @@ using TwitchChatOffset.CommandLine.Options;
 using TwitchChatOffset.Ytt;
 using System.CommandLine;
 using YTSubConverter.Shared;
+using static TwitchChatOffset.CommandLine.EnumSerialization;
 using static TwitchChatOffset.CommandLine.Options.OptionAliases;
 
 namespace TwitchChatOffset.CommandLine;
@@ -29,109 +30,109 @@ public static class Tokens
         HelpName = "Suffix to be apended to all output file names, including the extension"
     };
     
-    public static readonly Option<long> StartOption = new("start", Start.Aliases)
+    public static readonly Option<long> StartOption = new("--start", Start.Aliases)
     {
         HelpName = "Starting point in seconds before which to dismiss chat messages (optional)",
         DefaultValueFactory = _ => 0
     };
 
-    public static readonly Option<long> EndOption = new("end", End.Aliases)
+    public static readonly Option<long> EndOption = new("--end", End.Aliases)
     {
         HelpName = "Ending point in seconds after which to dismiss chat messages, or any negative number for no ending point (optional)",
         DefaultValueFactory = _ => -1
     };
 
-    public static readonly Option<long> DelayOption = new("delay", Delay.Aliases)
+    public static readonly Option<long> DelayOption = new("--delay", Delay.Aliases)
     {
         HelpName = "Delay in seconds to apply to all messages after cutting out unneeded messages (optional)",
         DefaultValueFactory = _ => 0
     };
 
-    public static readonly Option<Format> FormatOption = new("format", OptionAliases.Format.Aliases)
+    public static readonly Option<Format> FormatOption = new("--format", OptionAliases.Format.Aliases)
     {
-        HelpName = "Format for the output file (optional)",
+        HelpName = $"Format for the output file {Serialize<Format>()} (optional)",
         DefaultValueFactory = _ => default
     };
 
-    public static readonly Option<AnchorPoint> YttPositionOption = new("ytt-position", YttPosition.Aliases)
+    public static readonly Option<AnchorPoint> YttPositionOption = new("--ytt-position", YttPosition.Aliases)
     {
-        HelpName = "Position on the screen for YTT subtitles (ytt only) (optional)",
+        HelpName = $"Position on the screen for YTT subtitles {Serialize<AnchorPoint>()} (ytt only) (optional)",
         DefaultValueFactory = _ => AnchorPoint.TopLeft
     };
 
-    public static readonly Option<long> YttMaxMessagesOption = new("ytt-max-messages", YttMaxMessages.Aliases)
+    public static readonly Option<long> YttMaxMessagesOption = new("--ytt-max-messages", YttMaxMessages.Aliases)
     {
         HelpName = "Maximum number of messages to display at once for YTT subtitles (ytt only) (must be at least 1) (optional)",
         DefaultValueFactory = _ => 6
     };
 
-    public static readonly Option<long> YttMaxCharsPerLineOption = new("ytt-max-chars-per-line", YttMaxCharsPerLine.Aliases)
+    public static readonly Option<long> YttMaxCharsPerLineOption = new("--ytt-max-chars-per-line", YttMaxCharsPerLine.Aliases)
     {
         HelpName = "Maximum number of characters to display in a single line before it wraps to a new line (ytt only) (optional)",
         DefaultValueFactory = _ => 55
     };
 
-    public static readonly Option<double> YttScaleOption = new("ytt-scale", YttScale.Aliases)
+    public static readonly Option<double> YttScaleOption = new("--ytt-scale", YttScale.Aliases)
     {
         HelpName = "YTT subtitle size (e.g. 0, 0.5, 1.5, etc.) (ytt only) (must be at least 0) (optional)",
         DefaultValueFactory = _ => 0.0
     };
 
-    public static readonly Option<Shadow> YttShadowOption = new("ytt-shadow", YttShadow.Aliases)
+    public static readonly Option<Shadow> YttShadowOption = new("--ytt-shadow", YttShadow.Aliases)
     {
-        HelpName = "Shadow type (or none) for YTT subtitles (ytt only) (optional)",
+        HelpName = $"Shadow type (or none) for YTT subtitles {Serialize<Shadow>()} (ytt only) (optional)",
         DefaultValueFactory = _ => Shadow.Glow
     };
 
-    public static readonly Option<long> YttBackgroundOpacityOption = new("ytt-background-opacity", YttBackgroundOpacity.Aliases)
+    public static readonly Option<long> YttBackgroundOpacityOption = new("--ytt-background-opacity", YttBackgroundOpacity.Aliases)
     {
         HelpName = "Background opacity for YTT subtitles, ranging from 0 (fully transparent) to 255 (fully opaque) (ytt only) (optional)",
         DefaultValueFactory = _ => 0
     };
 
-    public static readonly Option<string> YttTextColorOption = new("ytt-text-color", YttTextColor.Aliases)
+    public static readonly Option<string> YttTextColorOption = new("--ytt-text-color", YttTextColor.Aliases)
     {
         HelpName = "Text color for YTT subtitles, (e.g. \"white\", \"#B0B0B0\", etc.) (ytt only) (optional)",
         DefaultValueFactory = _ => "white"
     };
 
-    public static readonly Option<string> YttShadowColorOption = new("ytt-shadow-color", YttShadowColor.Aliases)
+    public static readonly Option<string> YttShadowColorOption = new("--ytt-shadow-color", YttShadowColor.Aliases)
     {
         HelpName = "Shadow color for YTT subtitles, (e.g. \"black\", \"#B0B0B0\", etc.) (ytt only) (optional)",
         DefaultValueFactory = _ => "black"
     };
 
-    public static readonly Option<string> YttBackgroundColorOption = new("ytt-background-color", YttBackgroundColor.Aliases)
+    public static readonly Option<string> YttBackgroundColorOption = new("--ytt-background-color", YttBackgroundColor.Aliases)
     {
         HelpName = "Background color for YTT subtitles, (e.g. \"black\", \"#B0B0B0\", etc.) (ytt only) (optional)",
         DefaultValueFactory = _ => "black"
     };
 
-    public static readonly Option<string> OutputDirOption = new("output-dir", OutputDir.Aliases)
+    public static readonly Option<string> OutputDirOption = new("--output-dir", OutputDir.Aliases)
     {
         HelpName = "Output directory (will create if doesn't exist) (optional)",
         DefaultValueFactory = _ => "."
     };
     
-    public static readonly Option<string> InputDirOption = new("input-dir", InputDir.Aliases)
+    public static readonly Option<string> InputDirOption = new("--input-dir", InputDir.Aliases)
     {
         HelpName = "Input directory (optional)",
         DefaultValueFactory = _ => "."
     };
 
-    public static readonly Option<bool> QuietOption = new("quiet", Quiet.Aliases)
+    public static readonly Option<bool> QuietOption = new("--quiet", Quiet.Aliases)
     {
         HelpName = "Do not print a message for intermediate steps such as individual files being written (optional)",
         DefaultValueFactory = _ => false
     };
 
-    public static readonly Option<string> SearchPatternOption = new("search-pattern", SearchPattern.Aliases)
+    public static readonly Option<string> SearchPatternOption = new("--search-pattern", SearchPattern.Aliases)
     {
         HelpName = "Filter which files to transform by name; may contain wildcards '*' (zero or more characters) and '?' (exactly one character) (optional)",
         DefaultValueFactory = _ => "*.json"
     };
 
-    public static readonly Option<long> OptionPriorityOption = new("option-priority", OptionAliases.OptionPriority.Aliases)
+    public static readonly Option<long> OptionPriorityOption = new("--option-priority", OptionAliases.OptionPriority.Aliases)
     {
         HelpName = "Select priority to determine which options should be used when there is a clash between the CLI options and the CSV options (can be any integer; higher priority wins; if priorities are equal, then CSV is prioritised (optional)",
         DefaultValueFactory = _ => 0
