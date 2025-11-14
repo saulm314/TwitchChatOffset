d51e5f54de086a93154cb97b95cd4b8eb1d26927
Saulius Markevicius <saulius0412@gmail.com>
2025-11-14 19:29:54 +0000

    Optimise JToken sorting method

--- TwitchChatOffset/CommentComparer.cs
+++ TwitchChatOffset/CommentComparer.cs
@@ -8,14 +8,6 @@ public class CommentComparer : Comparer<JToken>
 {
     public static readonly CommentComparer Instance = new();
 
-    public override int Compare(JToken? x, JToken? y)
-    {
-        long diff = x!.D("content_offset_seconds").As<long>() - y!.D("content_offset_seconds").As<long>();
-        return diff switch
-        {
-            < 0 => -1,
-            > 0 => 1,
-            0 => 0
-        };
-    }
+    // if the chat times are more than 68 years apart, the conversion to an int will cause an overflow/underflow
+    public override int Compare(JToken? x, JToken? y) => (int)(x!.D("content_offset_seconds").As<long>() - y!.D("content_offset_seconds").As<long>());
 }
