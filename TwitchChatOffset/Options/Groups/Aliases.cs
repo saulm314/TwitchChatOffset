0d56ebca7f6c130f5d7d18aa7da7d1aac2e02bf5
Saulius Markevicius <saulius0412@gmail.com>
2025-11-17 23:15:35 +0000

    Add ASS option aliases

--- TwitchChatOffset/Options/Groups/Aliases.cs
+++ TwitchChatOffset/Options/Groups/Aliases.cs
@@ -8,17 +8,18 @@ public static class Aliases
     public static AliasesContainer Format { get; } = new(["--format", "--formatting", "-f"]);
     public static AliasesContainer SubPosition { get; } = new(["--subtitle-position", "--sub-position", "--ytt-position"]);
     public static AliasesContainer SubMaxMessages { get; } = new(["--subtitle-max-messages", "--sub-max-messages", "--ytt-max-messages"]);
-    public static AliasesContainer SubMaxCharsPerLine { get; } = new(["--subtitle-max-chars-per-line","--sub-max-chars-per-line","--ytt-max-chars-per-line", 
-       "--subtitle-max-chars", "--sub-max-chars", "--ytt-max-chars"]);
-    public static AliasesContainer SubScale { get; } = new(["--subtitle-scale", "--sub-scale", "--ytt-scale"]);
-    public static AliasesContainer SubShadow { get; } = new(["--subtitle-shadow", "--sub-shadow", "--ytt-shadow"]);
-    public static AliasesContainer SubWindowOpacity { get; } = new(["--subtitle-window-opacity", "--sub-window-opacity", "--ytt-window-opacity"]);
+    public static AliasesContainer SubMaxCharsPerLine { get; } = new(["--subtitle-max-chars-per-line","--sub-max-chars-per-line","--ytt-max-chars-per-line",
+        "--ass-max-chars-per-line", "--subtitle-max-chars", "--sub-max-chars", "--ytt-max-chars", "--ass-max-chars"]);
+    public static AliasesContainer SubScale { get; } = new(["--subtitle-scale", "--sub-scale", "--ytt-scale", "--ass-scale"]);
+    public static AliasesContainer SubShadow { get; } = new(["--subtitle-shadow", "--sub-shadow", "--ytt-shadow", "--ass-shadow"]);
+    public static AliasesContainer SubWindowOpacity { get; } = new(["--subtitle-window-opacity", "--sub-window-opacity", "--ytt-window-opacity",
+        "--ass-window-opacity"]);
     public static AliasesContainer SubBackgroundOpacity { get; } = new(["--subtitle-background-opacity", "--sub-background-opacity", "--ytt-background-opacity",
-        "--subtitle-bg-opacity", "--sub-bg-opacity", "--ytt-bg-opacity"]);
-    public static AliasesContainer SubTextColor { get; } = new(["--subtitle-text-color", "--sub-text-color", "--ytt-text-color"]);
-    public static AliasesContainer SubShadowColor { get; } = new(["--subtitle-shadow-color", "--sub-shadow-color", "--ytt-shadow-color"]);
+        "--ass-backgrond-opacity", "--subtitle-bg-opacity", "--sub-bg-opacity", "--ytt-bg-opacity", "--ass-bg-opacity"]);
+    public static AliasesContainer SubTextColor { get; } = new(["--subtitle-text-color", "--sub-text-color", "--ytt-text-color", "--ass-text-color"]);
+    public static AliasesContainer SubShadowColor { get; } = new(["--subtitle-shadow-color", "--sub-shadow-color", "--ytt-shadow-color", "--ass-shadow-color"]);
     public static AliasesContainer SubBackgroundColor { get; } = new(["--subtitle-background-color", "--sub-background-color", "--ytt-background-color",
-        "--subtitle-bg-color", "--sub-bg-color", "--ytt-bg-color"]);
+        "--ass-background-color", "--subtitle-bg-color", "--sub-bg-color", "--ytt-bg-color", "--ass-bg-color"]);
     public static AliasesContainer InputFile { get; } = new(["--input-file"]);
     public static AliasesContainer InputDir { get; } = new(["--input-directory", "--input", "-i"]);
     public static AliasesContainer OutputFile { get; } = new(["--output-file"]);
